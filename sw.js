363d1a6987dca1fb89411aca7e6d45ada22bec7f
sabchangasi <dhwanitpatel1@gmail.com>
2025-08-19 11:47:48 +0530

    first deploy

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'navratri-pass-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install the service worker and cache the app's files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response from cache
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
